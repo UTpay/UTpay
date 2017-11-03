cb8be9cac0be5d0b73873f8fa1c1e57cc9df3fc5
m1nam1 <info@minami.xyz>
2017-11-04 06:04:54 +0900

    Implement transfer UTCoin function

--- website/views.py
+++ website/views.py
@@ -18,16 +18,52 @@ class SendView(View):
     def get(self, request):
         eth_account = get_object_or_404(EthAccount, user=request.user)
 
+        # TODO: Set fee
+        fee = 0.001
+
         # Get balance of UTCoin
         web3 = Web3(HTTPProvider('http://localhost:8545'))
         abi = self.load_abi(settings.ARTIFACT_PATH)
         UTCoin = web3.eth.contract(abi=abi, address=settings.UTCOIN_ADDRESS)
-        balance = UTCoin.call().balanceOf(eth_account.address)
+        balance = UTCoin.call().balanceOf(eth_account.address) - fee
+        if balance < 0:
+            balance = 0
 
-        # TODO: Set fee
-        fee = 0.001
+        form = SendForm(user=request.user, initial={'fee': fee, 'balance': balance})
 
-        form = SendForm(initial={'fee': fee, 'balance': balance})
+        context = {
+            'title': 'コインを送る',
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        eth_account = get_object_or_404(EthAccount, user=request.user)
+        from_address = eth_account.address
+        num_suffix = 1000
+
+        form = SendForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            to_address = form.cleaned_data['address']
+            amount = int(form.cleaned_data['amount'] * num_suffix)
+
+            # TODO: Set fee
+            fee = 0.001
+
+            # Send UTCoin
+            web3 = Web3(HTTPProvider('http://localhost:8545'))
+            abi = self.load_abi(settings.ARTIFACT_PATH)
+            UTCoin = web3.eth.contract(abi=abi, address=settings.UTCOIN_ADDRESS)
+            if web3.personal.unlockAccount(from_address, eth_account.password, duration=hex(300)):
+                print('Account unlocked.')
+                try:
+                    tx_hash = UTCoin.transact({'from': from_address}).transfer(to_address, amount)
+                    print('tx_hash:', tx_hash)
+                    print('Transaction complete!')
+                except Exception as e:
+                    print(e)
+            else:
+                print('Failed to unlock account.')
 
         context = {
             'title': 'コインを送る',
