cb1e853f8a6cff52314b4b92467d2c1d21ce7002
m1nam1 <info@minami.xyz>
2017-11-03 21:26:05 +0900

    Create an EthAccount at sign up

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.views import View
+from django.db import transaction
+from web3 import Web3, HTTPProvider, IPCProvider
+import secrets
+import string
 
 from .models import *
 from .forms import SignUpForm
@@ -21,7 +25,18 @@ class SignUpView(View):
     def post(self, request):
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            # Save objects
+            with transaction.atomic():
+                # Create User
+                user = form.save()
+
+                # Create EthAccount
+                web3 = Web3(HTTPProvider('http://localhost:8545'))
+                password = self.make_random_password(length=30)
+                address = web3.personal.newAccount(password)
+                eth_account = EthAccount.objects.create(user=user, address=address, password=password)
+
+            # Log in
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
@@ -33,6 +48,11 @@ class SignUpView(View):
                 'form': form
             }
             return render(request, self.template_name, context)
+
+    def make_random_password(self, length):
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        return password
 
 @method_decorator(login_required, name='dispatch')
 class MyPageView(View):
