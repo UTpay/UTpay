0796ad5e21b7d73ebe80f9c735c607d0bbc19e52
m1nam1 <info@minami.xyz>
2017-11-03 18:37:35 +0900

    Create SignUpView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from .models import *
+from .forms import SignUpForm
+
+class SignUpView(View):
+    template_name = 'signup.html'
+
+    def get(self, request):
+        form = SignUpForm()
+        context = {
+            'title': 'Sign up',
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/') # mypage
+        else:
+            context = {
+                'title': 'Sign up',
+                'form': form
+            }
+            return render(request, self.template_name, context)
