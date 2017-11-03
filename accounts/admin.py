6ae08bdf564c5114ecc99ec4fb9e2a6d0592bfe6
m1nam1 <info@minami.xyz>
2017-11-03 21:23:25 +0900

    Fix admin.py of accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'address_upper', 'password', 'privatekey', 'created_at', 'modified_at')
+class EthAccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'address', 'password', 'created_at', 'modified_at')
     list_filter = ('user', 'created_at', 'modified_at')
     ordering = ('id',)
 
-admin.site.register(Account, AccountAdmin)
+admin.site.register(EthAccount, EthAccountAdmin)
