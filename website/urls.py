119cd30056abba2b57468ef7b12f36b47e320596
m1nam1 <info@minami.xyz>
2017-11-04 02:09:04 +0900

    Create SendView

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^send/$', SendView.as_view(), name='send')
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView
+from django.conf import settings
+from web3 import Web3, HTTPProvider
+import json
 
 from .models import *
+from accounts.models import EthAccount
+from .forms import *
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class SendView(View):
+    template_name = 'send.html'
+
+    def get(self, request):
+        eth_account = get_object_or_404(EthAccount, user=request.user)
+
+        # Get balance of UTCoin
+        web3 = Web3(HTTPProvider('http://localhost:8545'))
+        abi = self.load_abi(settings.ARTIFACT_PATH)
+        UTCoin = web3.eth.contract(abi=abi, address=settings.UTCOIN_ADDRESS)
+        balance = UTCoin.call().balanceOf(eth_account.address)
+
+        # TODO: Set fee
+        fee = 0.001
+
+        form = SendForm(initial={'fee': fee, 'balance': balance})
+
+        context = {
+            'title': 'コインを送る',
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def load_abi(self, file_path):
+        artifact = open(file_path, 'r')
+        json_dict = json.load(artifact)
+        abi = json_dict['abi']
+        return abi
