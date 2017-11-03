5c0ef5e3278c3941f230ba6ca4d404db5ba925a1
m1nam1 <info@minami.xyz>
2017-11-03 13:53:05 +0900

    Fix Account model

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Ethereum Account
 class Account(models.Model):
     user = models.ForeignKey(User, 'User')
-    address = models.CharField('Address', max_length=42)
+    address_lower = models.CharField('Address (lowercase)', max_length=42)
+    address_upper = models.CharField('Address (uppercase)', max_length=42)
     password = models.CharField('Password', max_length=30)
     privatekey = models.CharField('Private Key', max_length=64)
 
