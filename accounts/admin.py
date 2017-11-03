cee9e56fead504c797352f3574e2209399e269c9
m1nam1 <info@minami.xyz>
2017-11-03 18:30:19 +0900

    Create admin.py in accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'address_upper', 'password', 'privatekey', 'created_at', 'modified_at')
+    list_filter = ('user', 'created_at', 'modified_at')
+    ordering = ('id',)
+
+admin.site.register(Account, AccountAdmin)
