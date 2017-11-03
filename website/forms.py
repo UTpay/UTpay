e59bdbb729f6abc40c8e79c73e140e0753dcb9de
m1nam1 <info@minami.xyz>
2017-11-04 03:35:36 +0900

    Change attrs "disabled" to "readonly"

--- website/forms.py
+++ website/forms.py
@@ -9,8 +9,8 @@ from accounts.models import EthAccount
 class SendForm(forms.Form):
     address = forms.CharField(label='宛先', max_length=42, widget=forms.TextInput(attrs={'placeholder': '0x...'}))
     amount = forms.FloatField(label='金額 (UTC)', min_value=0.001, widget=forms.TextInput(attrs={'placeholder': '例) 10.123'}))
-    fee = forms.FloatField(label='手数料 (UTC)', widget=forms.TextInput(attrs={'disabled': 'disabled'}))
-    balance = forms.FloatField(label='送金可能額 (UTC)', min_value=0, widget=forms.TextInput(attrs={'disabled': 'disabled'}))
+    fee = forms.FloatField(label='手数料 (UTC)', widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    balance = forms.FloatField(label='送金可能額 (UTC)', min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
 
     def __init__(self, user, data=None, initial=None):
