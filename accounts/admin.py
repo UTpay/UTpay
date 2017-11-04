3dea3e2832947fe94dd6dd37fbe3d54bd5eb366b
m1nam1 <info@minami.xyz>
2017-11-04 23:18:22 +0900

    Create Transaction model in Accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,4 +7,10 @@ class EthAccountAdmin(admin.ModelAdmin):
     list_filter = ('user', 'created_at', 'modified_at')
     ordering = ('id',)
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'eth_account', 'tx_hash', 'from_address', 'to_address', 'amount', 'gas', 'gas_price', 'value', 'network_id', 'is_active', 'created_at')
+    list_filter = ('user', 'eth_account', 'from_address', 'to_address', 'amount', 'gas', 'gas_price', 'value', 'network_id', 'is_active', 'created_at')
+    ordering = ('id',)
+
 admin.site.register(EthAccount, EthAccountAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Ethereum Account
+# Ethereum account
 class EthAccount(models.Model):
     user = models.ForeignKey(User)
     address = models.CharField('Address', max_length=42)
@@ -16,3 +16,25 @@ class EthAccount(models.Model):
     class Meta:
         verbose_name = 'ETH Account'
         verbose_name_plural = 'ETH Accounts'
+
+# Transaction information
+class Transaction(models.Model):
+    user = models.ForeignKey(User)
+    eth_account = models.ForeignKey(EthAccount)
+    tx_hash = models.CharField('TxHash', max_length=66)
+    from_address = models.CharField('From', max_length=42)
+    to_address = models.CharField('To', max_length=42)
+    amount = models.BigIntegerField('Amount', help_text='UTC')
+    gas = models.BigIntegerField('Gas')
+    gas_price = models.BigIntegerField('Gas Price')
+    value = models.BigIntegerField('Value')
+    network_id = models.IntegerField('Network ID')
+    is_active = models.BooleanField('有効', default=True)
+    created_at = models.DateTimeField('作成日時', default=timezone.now)
+
+    def __str__(self):
+        return self.tx_hash
+
+    class Meta:
+        verbose_name = 'Transaction'
+        verbose_name_plural = 'Transactions'
