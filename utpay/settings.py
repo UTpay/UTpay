e8cf2c3b4851feb1e73451ff7b287b1b089693b4
m1nam1 <info@minami.xyz>
2017-11-04 21:37:18 +0900

    Fix Travis CI settings

--- utpay/settings.py
+++ utpay/settings.py
@@ -133,9 +133,16 @@ ARTIFACT_PATH = 'static/contracts/ropsten/UTCoin.json'
 UTCOIN_ADDRESS = '0x9bab3c293ca109de331069addced28380c5cc6fc'
 
 
+# Load all local settings
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+
 # Settings for Travis CI
 if 'TRAVIS' in os.environ:
-    SECRET_KEY="rp1=@2u^wjg)xsxql-lxf1zfx@q9dhzma@&vf3o5zzzzc5@ybq"
+    SECRET_KEY='rp1=@2u^wjg)xsxql-lxf1zfx@q9dhzma@&vf3o5zzzzc5@ybq'
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -148,10 +155,3 @@ if 'TRAVIS' in os.environ:
             },
         }
     }
-
-
-# Load all local settings
-try:
-    from .local_settings import *
-except ImportError:
-    pass
