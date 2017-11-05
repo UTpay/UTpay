48000ce6a760aeef553c5fb04ceb8d7795a37366
m1nam1 <info@minami.xyz>
2017-11-05 18:34:46 +0900

    Customize the auth.User admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .models import *
+
+UserAdmin.list_display = ('username', 'email', 'last_login', 'date_joined', 'is_active', 'is_staff')
+UserAdmin.ordering = ('id',)
 
 class EthAccountAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'address', 'password', 'created_at', 'modified_at')
@@ -12,5 +17,7 @@ class TransactionAdmin(admin.ModelAdmin):
     list_filter = ('user', 'eth_account', 'from_address', 'to_address', 'amount', 'gas', 'gas_price', 'value', 'network_id', 'is_active', 'created_at')
     ordering = ('id',)
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(EthAccount, EthAccountAdmin)
 admin.site.register(Transaction, TransactionAdmin)
