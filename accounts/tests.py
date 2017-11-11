1ab5cb5bb27b1de8c4ecb204e2b1e20524625861
m1nam1 <info@minami.xyz>
2017-11-11 15:18:20 +0900

    Create UserModelTests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from django.conf import settings
+from django.contrib.auth.models import User
+from web3 import Web3, HTTPProvider
+import secrets
+import string
+import qrcode
 
-# Create your tests here.
+from .models import *
+
+class UserModelTests(TestCase):
+    def create_user(self):
+        username = 'test'
+        email = 'test@example.com'
+        password = 'hogehoge'
+
+        # Create User
+        user = User.objects.create_user(username=username, email=email, password=password)
+
+        # Generate random password and address
+        web3 = Web3(HTTPProvider('http://localhost:8545'))
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for _ in range(30))
+        address = web3.personal.newAccount(password)
+
+        # Generate QR code
+        img = qrcode.make(address)
+        file_name = address + '.png'
+        file_path = '/images/qrcode/' + file_name
+
+        # Create EthAccount
+        eth_account = EthAccount.objects.create(user=user, address=address, password=password, qrcode=file_path)
+
+    def test_is_empty(self):
+        users = User.objects.all()
+        self.assertEqual(users.count(), 0)
+
+    def test_is_not_empty(self):
+        self.create_user()
+        users = User.objects.all()
+        self.assertEqual(users.count(), 1)
+
+    def test_user_size_equals_eth_account_size(self):
+        self.create_user()
+        users = User.objects.all()
+        eth_accounts = EthAccount.objects.all()
+        self.assertEqual(users.count(), eth_accounts.count())
+
+    def test_saving_and_retrieving_user(self):
+        self.create_user()
+        users = User.objects.all()
+        user = users[0]
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertEqual(user.check_password('hogehoge'), True)
