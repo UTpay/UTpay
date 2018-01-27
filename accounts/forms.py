746df2f1062ae441e4bd8f588422798b4ba0e0cd
m1nam1 <info@minami.xyz>
2018-01-28 00:05:44 +0900

    Rewrite the web UI with Material Design Lite

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,9 +6,32 @@ from django.forms import ModelForm
 from .models import Contract
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='この項目は任意です。')
-    last_name = forms.CharField(max_length=30, required=False, help_text='この項目は任意です。')
-    email = forms.EmailField(max_length=254, help_text='この項目は必須です。有効なメールアドレスを入力してください。')
+    username = forms.CharField(
+        max_length=150,
+        label='ユーザー名',
+        help_text='半角英数字、@/./+/-/_ で150文字以下にしてください。',
+    )
+    first_name = forms.CharField(
+        max_length=30,
+        label='名 (First name)',
+    )
+    last_name = forms.CharField(
+        max_length=30,
+        label='姓 (Last name)',
+    )
+    email = forms.EmailField(
+        max_length=254,
+        label='メールアドレス',
+        help_text='有効な東京大学のメールアドレス(u-tokyo.ac.jp)を入力してください。',
+    )
+    password1 = forms.CharField(
+        label='パスワード',
+        help_text='パスワードは最低8文字以上必要です。数字だけのパスワードやよく使われるパスワードにはできません。',
+    )
+    password2 = forms.CharField(
+        label='パスワード (確認)',
+        help_text='確認のため、再度同じパスワードを入力してください。',
+    )
 
     class Meta:
         model = User
