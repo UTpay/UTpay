a1f7d487a6baadde5f70ff724e619e8935a1380b
m1nam1 <info@minami.xyz>
2018-01-29 21:30:58 +0900

    Add activation URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.urls import path
 from .views import *
 
 app_name = 'accounts'
 
 urlpatterns = [
     url(r'^signup/$', SignUpView.as_view(), name='signup'),
+    path('activation/<key>/', ActivationView.as_view(), name='activation'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     url(r'^mypage/$', MyPageView.as_view(), name='mypage'),
