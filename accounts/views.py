55fa0d94fc58928fdb2ea71ed37e6cb2739d2a41
m1nam1 <info@minami.xyz>
2018-01-29 21:34:12 +0900

    Create ActivationView, and send activation email when sign up

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,11 @@ from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.db import transaction
+from django.urls import reverse
 from web3 import Web3, HTTPProvider
 import secrets
 import string
+import uuid
 
 from .models import *
 from .forms import *
@@ -27,8 +29,15 @@ class SignUpView(View):
         if form.is_valid():
             # Save objects
             with transaction.atomic():
-                # Create User
-                user = form.save()
+                # Create User (仮登録)
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+
+                # Create Activate
+                activate_key = self.create_activate_key()
+                activate = Activate(user=user, key=activate_key)
+                activate.save()
 
                 # Create EthAccount
                 web3 = Web3(HTTPProvider('http://localhost:8545'))
@@ -36,12 +45,16 @@ class SignUpView(View):
                 address = web3.personal.newAccount(password)
                 eth_account = EthAccount.objects.create(user=user, address=address, password=password)
 
-            # Log in
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('accounts:mypage')
+                # Send activation email
+                base_url = "/".join(request.build_absolute_uri().split("/")[:3])
+                activation_url = base_url + reverse('accounts:activation', args=[activate_key])
+                user.email_user(
+                    '[UTpay] Please verify your email',
+                    f'@{user.username} さん\n\nこの度は、UTpay にご登録いただきありがとうございます。\n以下のURLにアクセスして、登録を確認してください。\n\n{activation_url}\n\n--\nUTpay <https://utpay.net>\ninfo@utpay.net'
+                )
+                # TODO: リダイレクト先にメッセージを表示
+
+            return redirect('website:index')
         else:
             context = {
                 'title': 'サインアップ',
@@ -53,6 +66,27 @@ class SignUpView(View):
         alphabet = string.ascii_letters + string.digits
         password = ''.join(secrets.choice(alphabet) for _ in range(length))
         return password
+
+    # Create random string
+    def create_activate_key(self):
+        return uuid.uuid4().hex
+
+class ActivationView(View):
+    template_name = 'activation.html'
+
+    def get(self, request, key):
+        activate = get_object_or_404(Activate, key=key)
+        user = activate.user
+
+        # アカウントを有効化
+        if user.is_active == False:
+            user.is_active = True
+            user.save()
+
+        context = {
+            'title': '本登録完了',
+        }
+        return render(request, self.template_name, context)
 
 @method_decorator(login_required, name='dispatch')
 class MyPageView(View):
